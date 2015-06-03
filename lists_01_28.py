469edee666bac0fc01911289199c154823dc9bdc
Kirsten Korevaar <kirsten@eml.cc>
2015-06-03 16:32:28 -0400

    First commit, structure for exercises

---
+++ lists_01_28.py
@@ -0,0 +1,30 @@
+# Problems taken from https://sites.google.com/site/prologsite/prolog-problems/1
+
+#01 Find the last element of a list
+#02 Find the last but one element of a list
+#03 Find the k'th element of a list
+#04 Find the number of element in a list
+#05 Reverse a list
+#06 Find out if a list is a palindrome
+#07 Flatten a nested list 
+#08 Eliminate consecutive duplicates of list elements
+#09 Pack consecutive duplicates of list elements into sublists
+#10 Run Length encoding of a list 
+#11 Modified run length encoding
+#12 Decode a run length encoded list
+#13 Run Length encoding of a list (direct solution)
+#14 Duplicate the elements of a list
+#15 Duplicate the elements in a list a given number of times
+#16 Drop every Nth element from a list
+#17 Split a list into two parts, the length of the first part is given
+#18 Extract a slice from a list
+#19 Rotate a list N places to the left
+#20 Remove the Kth element from a list
+#21 Inset an element at a given position into a list
+#22 Create a list containing all integers within a given range
+#23 Extract a number of randomly selected elements from a list
+#24 Lotto: Draw N different random numbers from the set 1..M
+#25 Generate a random permutation of the elements of a list
+#26 Generate the combinations of K distinct objects chosen from the N elements of a list
+#27 Group the elements of a set into disjoint subsets
+#28 Sorting a list of lists according to length of sublists
