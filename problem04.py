71c7cc258d836e60808d427c49217af1adbd8981
Kirsten Korevaar <kirsten@eml.cc>
2015-06-30 16:08:26 -0400

    Use format for string formatting

--- problem04.py
+++ problem04.py
@@ -4,6 +4,6 @@ def num_elem(some_list):
     return len(some_list)
 
 if __name__ == "__main__":
-    print "len of [] is " + str(num_elem([]))
-    print "len of [1, 'a'] is "  + str(num_elem([1, 'a']))
-    print "len of [[1, 2, 3, 4], 'a'] is " +  str(num_elem([[1, 2, 3, 4], 'a']))
+    print "len of [] is {0}".format(num_elem([]))
+    print "len of [1, 'a'] is {0}".format(num_elem([1, 'a']))
+    print "len of [[1, 2, 3, 4], 'a'] is {0}".format(num_elem([[1, 2, 3, 4], 'a']))
