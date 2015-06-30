7945344c74a86d69ca8b55e1f50e887469faed0c
Kirsten Korevaar <kirsten@eml.cc>
2015-06-30 16:18:26 -0400

    Add tests

--- problem05.py
+++ problem05.py
@@ -3,5 +3,6 @@ def reverse(some_list):
     return list(reversed(some_list))
 
 if __name__ == "__main__":
-    print "reverse [1, 2, 3, 4] : ",
-    print reverse([1, 2, 3, 4])
+    print "reverse [1, 2, 3, 4] : {0}".format(reverse([1, 2, 3, 4]))
+    print "reverse [] : {0}".format(reverse([]))
+    print "reverse [[1, 2], [3, 4]] : {0}".format(reverse([[1, 2], [3, 4]]))
