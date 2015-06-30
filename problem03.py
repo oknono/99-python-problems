0eb74c6321e78f07f19e585bde7aec914656bb26
Kirsten Korevaar <kirsten@eml.cc>
2015-06-30 16:05:58 -0400

    use format for string formatting

--- problem03.py
+++ problem03.py
@@ -7,6 +7,6 @@ def find_k_element(some_list, index):
         return None
 
 if __name__ == "__main__":
-   print "1th element of [1,2,3,4] is " + str(find_k_element([1, 2, 3, 4], 1))
-   print "2nd element of [1,2,3,4] is " + str(find_k_element([1, 2, 3, 4], 2))  
-   print "5th element of [1,2,3,4] is " + str(find_k_element([1, 2, 3, 4], 5))   
+   print "1th element of [1,2,3,4] is {0}".format(find_k_element([1, 2, 3, 4], 1))
+   print "2nd element of [1,2,3,4] is {0}".format(find_k_element([1, 2, 3, 4], 2))  
+   print "5th element of [1,2,3,4] is {0}".format(find_k_element([1, 2, 3, 4], 5))   
