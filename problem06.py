f5c90d8925bc47cd4c934a05945e1bc9c5d6194d
Kirsten Korevaar <kirsten@eml.cc>
2015-06-30 16:20:33 -0400

    Update string formatting

--- problem06.py
+++ problem06.py
@@ -6,8 +6,6 @@ def palindrome(some_list):
     return problem05.reverse(some_list) == some_list
 
 if __name__ == "__main__":
-    print "anana is a palindrome: ",
-    print palindrome(['a', 'n', 'a', 'n', 'a'])
-    print "banana is a palindrome: ",
-    print palindrome(['b', 'a', 'n', 'a', 'n', 'a'])
+    print "anana is a palindrome: {0}".format(palindrome(['a', 'n', 'a', 'n', 'a']))
+    print "banana is a palindrome: {0}".format(palindrome(['b', 'a', 'n', 'a', 'n', 'a']))
 
