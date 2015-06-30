35436a52fd10bfddb824842ff6d5f344963b2954
Kirsten Korevaar <kirsten@eml.cc>
2015-06-30 16:25:09 -0400

    Use format for string formatting

--- problem07.py
+++ problem07.py
@@ -6,7 +6,6 @@ def flatten(some_list):
     return [item for inner_list in some_list for item in inner_list]
 
 if __name__ == "__main__":
-    print "flatten([ [1,2], [1,2,3,4]]) :",
-    print flatten([ [1,2], [1,2,3,4]])
-    print "flatten(['a', ['a', 'b'], 'c', [1]]):",
-    print flatten(['a', ['a', 'b'], 'c', '1'])
+    print "flatten([ [1,2], [1,2,3,4]]) : {0}".format(flatten([ [1,2], [1,2,3,4]]))
+    print "flatten(['a', ['a', 'b'], 'c', [1]]): {0}".format(flatten(['a', ['a', 'b'], 'c', '1']))
+    #print "flatten([ 1, [ 2, 3], 4, [1]]): {0}".format(flatten([ 1, [ 2, 3], 4, [1]]))
