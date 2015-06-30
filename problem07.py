39442d6eb4a0f3f6ff62a5aea16c32a8c6cd04d9
Kirsten Korevaar <kirsten@eml.cc>
2015-06-30 16:27:39 -0400

    import method flatten from module compiler.ast

--- problem07.py
+++ problem07.py
@@ -1,3 +1,4 @@
+import compiler.ast
 # Flatten a nested list 
 # This method does not work for a singleton integer
 # https://wiki.python.org/moin/ProblemSets/99%20Prolog%20Problems%20Solutions#Problem_7:_Flatten_a_nested_list_structure
@@ -8,4 +9,4 @@ def flatten(some_list):
 if __name__ == "__main__":
     print "flatten([ [1,2], [1,2,3,4]]) : {0}".format(flatten([ [1,2], [1,2,3,4]]))
     print "flatten(['a', ['a', 'b'], 'c', [1]]): {0}".format(flatten(['a', ['a', 'b'], 'c', '1']))
-    #print "flatten([ 1, [ 2, 3], 4, [1]]): {0}".format(flatten([ 1, [ 2, 3], 4, [1]]))
+    print "flatten([ 1, [ 2, 3], 4, [1]]): {0}".format(compiler.ast.flatten([ 1, [ 2, 3], 4, [1]]))
